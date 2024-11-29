d9fb055dff81d66036cd4bbe717ead80e6af785d
Dom <dom@dechiera.com>
2024-11-29 18:36:12 +0800

    Display current move message instead of button

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 function Square({ value, onSquareClick }) {
-  return(
-    <button 
-      className="square"
-      onClick={onSquareClick}
-    >
+  return (
+    <button className="square" onClick={onSquareClick}>
       {value}
     </button>
   );
 }
 
-function Board({xIsNext, squares, onPlay}) {
+function Board({ xIsNext, squares, onPlay }) {
   function handleClick(i) {
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
     const nextSquares = squares.slice();
     if (xIsNext) {
-      nextSquares[i] = "X";
+      nextSquares[i] = 'X';
     } else {
-      nextSquares[i] = "O";
+      nextSquares[i] = 'O';
     }
     onPlay(nextSquares);
   }
@@ -28,9 +25,9 @@ function Board({xIsNext, squares, onPlay}) {
   const winner = calculateWinner(squares);
   let status;
   if (winner) {
-    status = "Winner: " + winner;
+    status = 'Winner: ' + winner;
   } else {
-    status = "Next player " + (xIsNext ? "X" : "O");
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
   }
 
   return (
@@ -73,19 +70,25 @@ export default function Game() {
 
   const moves = history.map((squares, move) => {
     let description;
-    if (move > 0) {
-      description = "Go to move #" + move;
+    if (currentMove === move) {
+      description = 'You are at move #' + move;
+    } else if (move > 0) {
+      description = 'Go to move #' + move;
     } else {
-      description = "Go to game start";
+      description = 'Go to game start';
     }
     return (
       <li key={move}>
-        <button onClick={() => jumpTo(move)}>{description}</button>
+        { currentMove === move ?
+            <span>{description}</span>
+          :
+            <button onClick={() => jumpTo(move)}>{description}</button>
+        }
       </li>
     );
-  })
+  });
 
-  return(
+  return (
     <div className="game">
       <div className="game-board">
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
@@ -95,7 +98,7 @@ export default function Game() {
       </div>
     </div>
   );
-} 
+}
 
 function calculateWinner(squares) {
   const lines = [
@@ -106,11 +109,11 @@ function calculateWinner(squares) {
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
-    [2, 4, 6]
+    [2, 4, 6],
   ];
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
   }
